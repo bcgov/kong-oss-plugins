c4e56ae903b018bd42f9cca42e13666bcbf1e997
ikethecoder <ikethecoder@copeconsulting.ca>
2025-01-31 13:52:05 -0800

    upd to ghaction

--- testsuite/playwright.config.ts
+++ testsuite/playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ["list"],
-    ["json", { outputFile: "test-results.json" }],
+    ["json", { outputFile: "playwright-results/test-results.json" }],
     ["html", { open: "never" }],
   ],
 
