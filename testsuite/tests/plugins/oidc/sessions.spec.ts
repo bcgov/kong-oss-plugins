307c2849a48b63069af0e3036429258364c8c0fd
ikethecoder <ikethecoder@copeconsulting.ca>
2025-05-16 13:04:53 -0700

    dont skip samesite

--- testsuite/tests/plugins/oidc/sessions.spec.ts
+++ testsuite/tests/plugins/oidc/sessions.spec.ts
@@ -42,11 +42,6 @@ test.describe("oidc plugin - happy paths", () => {
   });
 
   test("samesite strict", async ({ page, request }) => {
-    if ((process.env.KONG_VERSION || "").startsWith("2.")) {
-      console.warn("Skipping test - test fails for Kong 2");
-      return;
-    }
-
     await runE2Etest(
       page,
       request,
