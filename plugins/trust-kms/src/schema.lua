b29af2c6dde01472f5cd84f3f8c06896af92efbb
ikethecoder <ikethecoder@copeconsulting.ca>
2026-01-22 21:23:39 -0800

    fix kms undo schema change

--- plugins/trust-kms/src/schema.lua
+++ plugins/trust-kms/src/schema.lua
@@ -9,51 +9,6 @@ return {
         type = "record",
         fields = {
           {
-            direction = {
-              type = "string",
-              one_of = {
-                "request",
-                "response"
-              }
-            }
-          },
-          {
-            jwks_uri = {
-              type = "string"
-            }
-          },
-          {
-            signature_header_key = {
-              type = "string"
-            }
-          },
-          {
-            private_key_location = {
-              type = "string",
-              required = true
-            }
-          },
-          {
-            alg = {
-              type = "string",
-              one_of = {
-                "RS256",
-                "RS512",
-                "ES256",
-                "ES512"
-              }
-            }
-          },
-          {
-            hash_alg = {
-              type = "string",
-              one_of = {
-                "sha256",
-                "sha512"
-              }
-            }
-          },
-          {
             operation = {
               type = "string",
               default = "create_key",
